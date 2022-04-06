78323035eb0f294f8826e0ece53a60f127cd8af3
Gerardo Gil <gerardo.gil@gofore.com>
2022-04-06 11:31:54 +0200

    update ngrx-store-logger to a custom logger metareducer

--- src/app/store/reducers/app-meta.reducers.ts
+++ src/app/store/reducers/app-meta.reducers.ts
@@ -1,11 +1,19 @@
+import { environment } from '@/environments/environment';
 import { ActionReducer, MetaReducer } from '@ngrx/store';
-import { environment } from '@environment';
-import { storeLogger } from 'ngrx-store-logger';
-import { AppState } from '@/app/store/states/app.state';
+import { AppState } from '../states/app.state';
 
-export function logger(reducer: ActionReducer<any>): any {
-  // default, no options
-  return storeLogger()(reducer);
+/**
+ * Logger reducer used for debugging to log state changes in console.
+ */
+ export function loggerMetaReducer(reducer: ActionReducer<AppState>): ActionReducer<AppState> {
+  return (state: AppState | undefined, action: any): any => {
+    const result = reducer(state, action);
+    console.groupCollapsed(action.type);
+    console.log('prev state', state);
+    console.log('action', action);
+    console.log('next state', result);
+    console.groupEnd();
+    return result;
+  };
 }
-
-export const metaReducers: MetaReducer<AppState>[] = environment.production ? [] : [logger];
+export const metaReducers: MetaReducer<AppState>[] = environment.production ? [] : [loggerMetaReducer];
